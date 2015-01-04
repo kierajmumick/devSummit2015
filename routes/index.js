61af7469e0942b24389c88b0fc3ac625c3e07c40
Ubuntu <ubuntu@ip-172-31-33-202.ec2.internal>
2015-01-04 13:45:01 +0000

    lots of cool shit

--- routes/index.js
+++ routes/index.js
@@ -131,7 +131,7 @@ exports.message911 = function(req, res) {
 	    });
 	}
 
-	sendSMS(phone_number, "<<First Name >> <<Last name>> has had a heart attack event at lat 45.6789  long 123.4567 on 1/3/2015 at 9:45 pm. (Sent automatically from Mi Link Heart Rate Tracking System)", false);
+	sendSMS(phone_number, "<<First Name >> <<Last name>> has had a heart attack event at lat 44.6789  long 123.4567 on 1/3/2015 at 9:45 pm. (Sent automatically from Mi Link Heart Rate Tracking System)", false);
 }
 
 exports.allowAccess = function(req, res) {
